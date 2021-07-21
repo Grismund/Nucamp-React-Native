5b3a017b72f7c66a22a42828524b5cac331cc408
Seth Faerber <sethfaerber@yahoo.com>
2021-07-21 15:07:11 -0500

    git add .! Home Componenet...content added. git add .! Refer for Open-Art Categories page

--- components/HomeComponent.js
+++ components/HomeComponent.js
@@ -16,4 +16,64 @@ class Home extends Component {
     }
 }
 
+export default Home;import React, { Component } from 'react';
+import { View, Text, ScrollView } from 'react-native';
+import { Card } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
+import { PROMOTIONS } from '../shared/promotions';
+import { PARTNERS } from '../shared/partners';
+
+
+// USE THIS RENDERITEM FOR MY CATEGORIES FEATURE ON OPEN-ART.
+function RenderItem({item}) {
+    if (item) {
+        return (
+            <Card
+                featuredTitle={item.name}
+                image={require('./images/react-lake.jpg')}
+            >
+                <Text style={{margin: 10}}>
+                    {item.description}
+                </Text>
+            </Card>
+        );
+    }
+    return <View />;
+}
+
+class Home extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES,
+            promotions: PROMOTIONS,
+            partners: PARTNERS
+        };
+    }
+
+    static navigationOptions = {
+        title: 'Home'
+    }
+
+    render() {
+        return (
+            // ScrollView vs. FlatList. ScrollView renders EVERYTHING into memory then slides up and down. 
+            // FlatList uses 'lazy loading' which only renders what is displayed, so it renders as the element
+            // comes into view, then wipes it from memory when it goes out of view.
+            <ScrollView>
+                <RenderItem 
+                    item={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                />
+                <RenderItem 
+                    item={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                />
+                <RenderItem 
+                    item={this.state.partners.filter(partner => partner.featured)[0]}
+                />
+            </ScrollView>
+        );
+    }
+}
+
 export default Home;
