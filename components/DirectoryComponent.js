670358d37c396ca9150b3109061aa8f60ef7faac
Seth Faerber <sethfaerber@yahoo.com>
2021-07-21 11:01:56 -0500

    Stack Navigation added. Site reworked.

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
 
-function Directory(props) {
+// class components are useful for adding a constructor to hold data in state.
+class Directory extends Component {
 
-    const renderDirectoryItem = ({item}) => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES
+        };
+    }
+    // static keyword sets a method on the class itself rather than
+    // the object instantiated in the class.
+    // this particular usage allows a title to appear in the navigation
+    // when this Directory component is called from React-Navigation.
+    static navigationOptions = {
+        title: 'Directory'
+    }
+
+    render() {
+        // I dont know what's going on here. Why destructuring? Syntax?
+        const { navigate } = this.props.navigation;
+        const renderDirectoryItem = ({item}) => {
+            return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
+                    leftAvatar={{ source: require('./images/react-lake.jpg')}}
+                />
+            );
+        };
+
         return (
-            //syntax for ListItem is in documentation
-            <ListItem
-                title={item.name}
-                subtitle={item.description}
-                onPress={() => props.onPress(item.id)}
-                leftAvatar={{ source: require('./images/react-lake.jpg')}}
+            <FlatList
+                data={this.state.campsites}
+                renderItem={renderDirectoryItem}
+                keyExtractor={item => item.id.toString()}
             />
         );
-    };
-
-    return (
-        //syntax for FlatList is in documentation
-        <FlatList
-            data={props.campsites}
-            renderItem={renderDirectoryItem}
-            keyExtractor={item => item.id.toString()}
-        />
-    );
+    }
 }
 
 export default Directory;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,33 +1,48 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
-import { View } from 'react-native';
-import { CAMPSITES } from '../shared/campsites';
+import Constants from 'expo-constants';
+import { View, Platform } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+
+const DirectoryNavigator = createStackNavigator(
+    // in this argument, add an object which contains all of the 
+    // components that we will want to display. This is the 
+    // "route configs object."
+    {
+        Directory: { screen: Directory },
+        CampsiteInfo: { screen: CampsiteInfo }
+    },
+    // The optional second object does additional configuration.
+    // This lets you set up the menu color/size/text etc. 
+    {
+        initialRouteName: 'Directory',
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+// You will usually wrap your top level navigator for your app with createAppContainer.
+const AppNavigator = createAppContainer(DirectoryNavigator);
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES,
-            selectedCampsite: null
-        };
-    }
-
-    onCampsiteSelect(campsiteId) {
-        this.setState({selectedCampsite: campsiteId});
-    }
-
     render() {
         return (
-            <View style={{flex: 1}}>
-                <Directory
-                    campsites={this.state.campsites}
-                    onPress={campsiteId => this.onCampsiteSelect(campsiteId)}
-                />
-                <CampsiteInfo
-                    campsite={this.state.campsites.filter(
-                        campsite => campsite.id === this.state.selectedCampsite)[0]}
-                />
+            <View
+                style={{
+                    flex: 1,
+                    // Ternary operator to set padding differenly for different devices
+                    paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight
+            }}>
+                <AppNavigator />
             </View>
         );
     }
