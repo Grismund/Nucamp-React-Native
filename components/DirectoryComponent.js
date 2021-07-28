0134faa70d949bb666d96396232bc7b3b3245e39
Seth Faerber <sethfaerber@yahoo.com>
2021-07-27 21:34:39 -0500

    Redux fully working

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements';
-import { CAMPSITES } from '../shared/campsites';
+import { Tile } from 'react-native-elements';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        campsites: state.campsites,
+    };
+};
 
 // class components are useful for adding a constructor to hold data in state.
 class Directory extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            campsites: CAMPSITES
-        };
-    }
+
     // static keyword sets a method on the class itself rather than
     // the object instantiated in the class.
     // this particular usage allows a title to appear in the navigation
@@ -25,19 +27,20 @@ class Directory extends Component {
         const { navigate } = this.props.navigation;
         const renderDirectoryItem = ({item}) => {
             return (
-                //Use this to get a new card for each new object
-                <ListItem
+                //USE THIS TO GET A NEW CARD FOR EACH OBJECT
+                <Tile
                     title={item.name}
-                    subtitle={item.description}
+                    caption={item.description}
+                    featured
                     onPress={() => navigate('CampsiteInfo', { campsiteId: item.id })}
-                    leftAvatar={{ source: require('./images/react-lake.jpg')}}
+                    imageSrc={{uri: baseUrl + item.image}}
                 />
             );
         };
 
         return (
             <FlatList
-                data={this.state.campsites}
+                data={this.props.campsites.campsites}
                 renderItem={renderDirectoryItem}
                 keyExtractor={item => item.id.toString()}
             />
@@ -45,4 +48,4 @@ class Directory extends Component {
     }
 }
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
