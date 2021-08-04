ead4c5f84c379e8fe789c4d7faffe8f24bd51aa0
Seth Faerber <sethfaerber@yahoo.com>
2021-08-04 18:37:16 -0500

    Favorites component added

--- redux/comments.js
+++ redux/comments.js
@@ -8,6 +8,14 @@ export const comments = (state = { errMess: null, comments: []}, action) => {
         case ActionTypes.COMMENTS_FAILED:
             return {...state, errMess: action.payload};
 
+        // case ActionTypes.ADD_COMMENT:
+        //     return {...state, comment: action.payload, id: this.length + 1}
+        case ActionTypes.ADD_COMMENT:
+                const comment = action.payload;
+                comment.id = state.comments.length;
+                return{...state, comments: state.comments.concat(comment)}
+            
+
         default:
             return state;
     }
